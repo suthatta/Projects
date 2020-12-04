8f98c3546471ee048fa4ac10525e39fe78255f4f
Suthatta Lindstrii <suthatta.lindstrii@edp.se>
2020-12-04 22:59:19 +0100

    add a secound day code

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 let pieRepo = require('./repos/pieRepos');  //get  all data from file repos
 let addPieRepos = require('./repos/addPieRepos');
+let searchPieRepo = require('./repos/sPieRepos');
+let sPieRepo = require('./repos/singelPieRepos');
 //Use the express Router object
 let router = express.Router();
 
@@ -11,7 +13,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
-router.get('/', function (req, res, next){
+router.get('/api', function (req, res, next){
     pieRepo.get(function (data) {
         res.status(200).json({
             "status": 200,
@@ -24,7 +26,32 @@ router.get('/', function (req, res, next){
     });
    
 });
-
+//get a single id
+router.get('/api/:id', function (req, res, next){
+    sPieRepo.getById(req.params.id, function (data) {
+        if(data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "Single pie retrieved",
+                "data": data
+            });
+        }
+        else {
+            res.status(404).json( {
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pie '" + req.params.id + "' counld not be found.",
+                "error": {
+                    "code": "Not found",
+                    "message": "The pie '" + req.params.id + "' cound not be found."
+                }   
+            });
+        }        
+    }, function(err) {
+        next(err);
+    });   
+});
 
 router.post('/api', function(req, res, next){
     addPieRepos.insert(req.body, function(data) {
@@ -39,6 +66,78 @@ router.post('/api', function(req, res, next){
     });
 })
 
+
+router.put('/:id', function(req, res, next){
+    addPieRepos.getById(req.params.id, function(data) {
+        if(data){
+                //Attempt to update the data
+                addPieRepos.update(req.body, req.params.id, function(data){
+                    res.status(200).json({
+                        "status": 200,
+                        "statusText": "Updated Ok",
+                        "message": "New updated Pie",
+                        "data": data  
+                    });
+                });
+        }
+      else {
+          res.status(404).json({
+            "status": 404,
+            "statusText": "Not Found",
+            "message": "The pid '" + req.params.id + "' cound not be found.",
+            "error": {
+                "code": "Not_Found",
+                "message": "The pie '" + req.params.id + "' cound not be found."
+            }
+          });
+      }}, function(err) {
+        next(err);
+      });
+})
+
+
+router.delete('/:id', function(req, res, next){
+    addPieRepos.getById(req.params.id, function (data) {
+        if (data) {
+            addPieRepos.delete(req.params.id, function (data){
+                res.status(200).json( {
+                    "status": 200,
+                    "statusText": "Ok",
+                    "message": "The pie '" + req.params.id + "' is deleted.",
+                    "data": "Pie '" + req.params.id + "' deleted."  
+                });
+            })
+        } else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pid '" + req.params.id + "' cound not be found.",
+                "error": {
+                    "code": "Not_Found",
+                    "message": "The pie '" + req.params.id + "' cound not be found."
+                }
+              });
+          }
+        }, function(err) {
+            next(err);
+          });    
+})
+
+router.patch('/:id', function(req, res, next) {
+    addPieRepos.getById(req.params.id, function (data) {
+        if(data) {
+            //Attempt to update the data
+            addPieRepos.update(req.body, req.params.id, function(data) {
+                res.status(200).json ({
+                    "status": 200,
+                        "statusText": "Updated Ok",
+                        "message": "New updated Pie",
+                        "data": data  
+                });
+            })
+        }
+    })
+})
 
 
 
--- RestAPI/repos/addPieRepos.js
+++ RestAPI/repos/addPieRepos.js
@@ -56,7 +56,54 @@ let addPieRepo = {
                 });
             }
         });
-        }
+        },
+
+    update: function(newData, id, resolve, reject)  {
+        fs.readFile(FILE_NAME, function(err, data){
+            // console.log("read data ", JSON.parse(data));
+             if (err) {
+                 reject(err);
+             }
+             else {
+                 let pies = JSON.parse(data);
+                 let pie = pies.find(p => p.id == id);
+                 if(pie){
+                     Object.assign(pie, newData);
+                     fs.writeFile(FILE_NAME, JSON.stringify(pies), function(err){
+                        if(err){
+                            reject(err);
+                        }
+                        else {
+                            resolve(newData);
+                           // console.log("read new data ", newData);
+                        }
+                     });
+                    }
+                }
+            });  
+        },
+
+        delete: function(id, resolve, reject) {
+            fs.readFile(FILE_NAME, function(err, data){
+                // console.log("read data ", JSON.parse(data));
+                 if (err) {
+                     reject(err);
+                 }
+                 else {
+                     let pies = JSON.parse(data);
+                     let index = pies.findIndex(p => p.id == id);
+                     if(index != -1){
+                         pies.splice(index, 1);
+                         fs.writeFile(FILE_NAME, JSON.stringify(pies), function (err){
+                             if(err) {
+                                 reject(err);
+                             }
+                             else { resolve(index)};
+                         })
+                     }
+                 }
+        })
+    }
     };
     
 
